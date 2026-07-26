f124142f3238cf4c72f7d08af6027a9bc67251e0
buhOS <buhos.c3@gmail.com>
2026-07-25 22:03:31 -0500

    Add files via upload

---
+++ Day9_productInformation.c
@@ -0,0 +1,40 @@
+/************************************************************************
+* Name: productInformation.c                                            *
+* Purpose: Product information entered by the user                      *
+* Author: buhOS                                                         *
+* Date: 25/07/2026                                                      *
+*************************************************************************/
+
+#include <stdio.h>
+
+// Function prototypes
+void sayHelloBuhOS(const int day);
+
+// Main function
+int main(void)
+{
+    int item, day, month, year;
+    float price;
+    printf("--- PRODUCT INFORMATION 📝 ---\n");
+    printf("Enter item number: ");
+    scanf("%d", &item);
+    printf("Enter unit price: ");
+    scanf("%f", &price);
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+    printf("\nItem\t\tUnit\t\tPurchase\n"
+            "\t\tPrice\t\tDate\n"
+            "%d\t\t$ %.2f\t\t%.2d/%.2d/%.4d\n",
+            item, price, month, day, year);
+
+    sayHelloBuhOS(9);
+    return 0;               //or exit(0)
+}
+
+// Functions
+void sayHelloBuhOS(const int day)
+{
+    printf("\nHello, I'm buhOS🦉\n---Just keep going\n");
+    printf("---Day %i\n", day);
+    printf("\a");       // Alert (bell)
+}
